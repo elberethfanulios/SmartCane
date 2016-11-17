a4f2e006e973baddb5a300e8461a1faf95324f04
eleberethfanulios <yasmina.alkhalil@gmail.com>
2016-11-17 22:33:45 +0400

    add

---
+++ ArduinoCode/DFRobot_Bluno_Hello_World.ino
@@ -0,0 +1,71 @@
+
+#include <Wire.h>
+#define    LIDARLite_ADDRESS   0x62          // Default I2C Address of LIDAR-Lite.
+#define    RegisterMeasure     0x00          // Register to write to initiate ranging.
+#define    MeasureValue        0x04          // Value to initiate ranging.
+#define    RegisterHighLowB    0x8f          // Register to get both High and Low 
+//
+// Copyright 2014, Andreas Lundquist
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// DFRobot - Bluno - Hello World
+//                                               version: 0.2 - 2014-12-01
+//THIS ONE
+int reading = 0;
+void setup() {
+  Serial.begin(115200);
+Wire.begin(); 
+  pinMode(2, OUTPUT);
+  pinMode(A5, INPUT);     
+       
+}
+
+void loop() {
+
+
+  Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
+  Wire.write((int)RegisterMeasure); // sets register pointer to  (0x00)  
+  Wire.write((int)MeasureValue); // sets register pointer to  (0x00)  
+  Wire.endTransmission(); // stop transmitting
+
+  delay(20); // Wait 20ms for transmit
+
+  Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
+  Wire.write((int)RegisterHighLowB); // sets register pointer to (0x8f)
+  Wire.endTransmission(); // stop transmitting
+
+  delay(20); // Wait 20ms for transmit
+
+  Wire.requestFrom((int)LIDARLite_ADDRESS, 2); // request 2 bytes from LIDAR-Lite
+
+  if(2 <= Wire.available()) // if two bytes were received
+  {
+    reading = Wire.read(); // receive high byte (overwrites previous reading)
+   reading = reading << 8; // shift high byte to be high 8 bits
+    reading |= Wire.read(); // receive low byte as lower 8 bits
+
+   
+    }
+  byte buffer[1] = {(byte)reading};               
+  Serial.write(buffer, sizeof(buffer));
+Serial.println(reading);
+
+delay(200);}
+
+
+
+
+
+
+
